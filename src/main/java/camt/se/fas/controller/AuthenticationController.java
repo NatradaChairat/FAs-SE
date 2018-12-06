7d1a97d4b6b93b0f5e322f2813110258619e934b
Siriganya Sensupa <siriganya_sen@cmu.ac.th>
2018-12-07 00:28:15 +0700

    Merge branch 'master' of https://github.com/NatradaChairat/FAs-SE
    # Conflicts:
    #	client/package-lock.json

--- src/main/java/camt/se/fas/controller/AuthenticationController.java
+++ src/main/java/camt/se/fas/controller/AuthenticationController.java
@@ -1,14 +1,16 @@
 package camt.se.fas.controller;
 
 
-import camt.se.fas.entity.Account;
 import camt.se.fas.service.PythonService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin
 @RestController("/auth")
@@ -24,12 +26,8 @@ public class AuthenticationController {
     @PostMapping("/auth/faceLogin")
     public ResponseEntity faceLogin(@RequestParam String imageUrl) {
         try {
-
-<<<<<<< Updated upstream
             pythonService.runScript(imageUrl,"");
-=======
             pythonService.runScript("","");
->>>>>>> Stashed changes
             return ResponseEntity.ok().build();
         }  catch (Exception e) {
             e.printStackTrace();
